60a1d77fbd763823d187e6a140a997d281db47ac
Bazivar <3KwV4tnZDpiifgY>
2021-11-06 14:39:34 +0300

    Made some first articles. Met some trouble with generating the articles page from the pre-made html file. Will manage it later

--- Website/main/urls.py
+++ Website/main/urls.py
@@ -4,4 +4,6 @@ from . import  views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('articles', views.articles, name = 'articles'),
+    path('articles/article_integration', views.article_integration, name = 'article_integration'),
+    path('articles/article_vulner', views.article_vulner, name = 'article_vulner'),
 ]
